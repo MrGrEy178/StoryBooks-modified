1e8a3099f10b20fa775571644f8f37cea963c32f
mrgrey178 <ss4458ss51@gmail.com>
2021-09-13 16:54:31 +0400

    some small changes in hbs

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,26 @@ router.get('/', ensureGuest, (req, res) => {
 });
 
 router.get('/dashboard', ensureAuth, (req, res) => {
-    res.render('dashboard');
+    res.render('dashboard', {
+        posts: [
+            {
+                title: 'first',
+                content: 'lorem ipsum dolor sit amet'
+            },{
+                title: 'second',
+                content: 'lorem ipsum dolor sit amet'
+            },{
+                title: 'third',
+                content: 'lorem ipsum dolor sit amet'
+            },{
+                title: 'forth',
+                content: 'lorem ipsum dolor sit amet'
+            },{
+                title: 'fifth',
+                content: 'lorem ipsum dolor sit amet'
+            }
+        ]
+    });
 });
 
 router.get('/logout', (req, res) => {
