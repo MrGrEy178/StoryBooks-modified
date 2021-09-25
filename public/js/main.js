1e4f8090d3ccf8645cda2434e87e17b1cc1c148e
MrGrey178 <ss4458ss51@gmail.com>
2021-09-25 15:52:38 +0400

    single and stories by user

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-let tableRows = document.querySelectorAll('.body_rows');
-for(let i = 0; i < tableRows.length; i++){
-    if(i % 2 !== 0){
-        tableRows[i].style.backgroundColor = 'lightgray';
-    }else{
-        continue;
-    }
-}
--- routes/stories.js
+++ routes/stories.js
@@ -22,12 +22,20 @@ router.post('/', ensureAuth, async (req, res) => {
     }
 });
 
-// get request to the story
+// get request to the single story
 router.get('/:id', ensureAuth, async (req, res) => {
     let story = await Stories.findById(req.params.id).populate('author').lean();
     res.render('stories/show', {
         story,
     });
+});
+
+// show all public stories from user or all stories by auth user
+router.get('/users/:id', ensureAuth, async (req, res) => {
+    let stories = await Stories.find({"author": req.params.id}).lean();
+    res.render('stories/index', {
+        stories,
+    })
 });
 
 module.exports = router;
